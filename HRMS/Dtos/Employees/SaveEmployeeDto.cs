8e29d3acdd6a72f5be9cbc0917d8d1a288cf45e5
Qais-ir <qais.irshaid77@gmail.com>
2026-05-12 20:35:37 +0300

    Add project files.

---
+++ HRMS/Dtos/Employees/SaveEmployeeDto.cs
@@ -0,0 +1,17 @@
+﻿namespace HRMS.Dtos.Employees
+{
+    public class SaveEmployeeDto
+    {
+        public long? Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Position { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }// 07, +962789
+        public bool IsActive { get; set; }
+        public DateTime StartDate { get; set; } // Required
+        public DateTime? EndDate { get; set; } // (?) => Opteinal / Nullable
+        public decimal? Salary { get; set; }
+    }
+}
---
+++ HRMS/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace HRMS.Models
+{
+    public class Employee
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Position { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }// 07, +962789
+        public bool IsActive { get; set; }
+        public DateTime StartDate { get; set; } // Required
+        public DateTime? EndDate { get; set; } // (?) => Opteinal / Nullable
+        public decimal? Salary { get; set; }
+
+    }
+}
