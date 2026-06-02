a950be97cd80ced588cfbf47e690c0b41ad65a77
Qais-ir <qais.irshaid77@gmail.com>
2026-06-02 21:47:00 +0300

    Code First Migrations

--- HRMS/Models/Employee.cs
+++ HRMS/Models/Employee.cs
@@ -1,21 +1,38 @@
-﻿namespace HRMS.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HRMS.Models
 {
     public class Employee
     {
+        [Key]
         public long Id { get; set; }
+        [MaxLength(50)]
         public string FirstName { get; set; }
+        [MaxLength(50)]
         public string LastName { get; set; }
+        [MaxLength(50)]
         public string Email { get; set; }
+        [MaxLength(50)]
         public string Position { get; set; }
         public DateTime BirthDate { get; set; }
+        [MaxLength(50)]
         public string PhoneNumber { get; set; }// 07, +962789
         public bool IsActive { get; set; }
         public DateTime StartDate { get; set; } // Required
         public DateTime? EndDate { get; set; } // (?) => Opteinal / Nullable
         public decimal? Salary { get; set; }
-        public long? DepartmentId { get; set; }
-        public long? ManagerId { get; set; }
 
+        [ForeignKey("Department")]
+        public long? DepartmentId { get; set; }
+        public Department? Department { get; set; } // Navigation Property
+
+        [ForeignKey("Manager")]
+        public long? ManagerId { get; set; }
+        public Employee? Manager { get; set; } // Navigation Property
+
+
+        //public ICollection<Employee>? Employees { get; set; }
 
     }
 }
